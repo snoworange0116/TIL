cbc0ab92666e8916c8a35d6ce15847f900ea96e8
wooyoung choi <rudolp3@gmail.com>
2020-02-27 18:13:25 +0900

    programmers algorithm problem

--- Algorithm_Problem/ETC/programmers/secret_map.py
+++ Algorithm_Problem/ETC/programmers/secret_map.py
@@ -22,10 +22,19 @@ def solution(n, arr1, arr2):
                 sum_tmp += ' '
         answer.append(sum_tmp)
     return answer
-# n=5
-# arr1 = [9, 20, 28, 18, 11]
-# arr2 = [30, 1, 21, 17, 28]
+
 n=6
 arr1 = [46, 33, 33 ,22, 31, 50]
 arr2 = [27 ,56, 19, 14, 14, 10]
 print(solution(n,arr1,arr2))
+
+# 모범답안
+# def solution(n, arr1, arr2):
+#     answer = []
+#     for i,j in zip(arr1,arr2):
+#         a12 = str(bin(i|j)[2:])
+#         a12=a12.rjust(n,'0')
+#         a12=a12.replace('1','#')
+#         a12=a12.replace('0',' ')
+#         answer.append(a12)
+#     return answer
